cd3bcbc3e8223991b681dc85cb3c7212f208a399
Jacob Mortensen <jacob@yourmemoir.com>
2015-07-20 22:53:34 -0400

    switch to Service from IntentService b/c need to stay awake for Hue callbacks

--- mobile/src/main/java/huewear/huewear/HueService.java
+++ mobile/src/main/java/huewear/huewear/HueService.java
@@ -1,7 +1,14 @@
 package huewear.huewear;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -18,6 +25,7 @@ import com.philips.lighting.model.PHHueParsingError;
 import com.philips.lighting.model.PHLight;
 import com.philips.lighting.model.PHLightState;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -25,7 +33,7 @@ import java.util.Random;
 /**
  * Created by jacob on 7/19/15.
  */
-public class HueService extends IntentService implements PHSDKListener, PHLightListener {
+public class HueService extends Service implements PHSDKListener, PHLightListener {
 	private static final String TAG = "HueService";
 	private static final int MAX_HUE=65535;
 	public static final String POINTS_FOUND = "HueService.points_found";
@@ -35,20 +43,70 @@ public class HueService extends IntentService implements PHSDKListener, PHLightL
 	public static final String ERROR = "HueService.error";
 	public static final String ACCESS_POINT_EXTRA = "HueService.access_point_extra";
 
+	private List<Integer> allIds; //some Hue callbacks can't tell which it was called from- stop all
+	private List<Integer> randomLightIds;
+	private List<Integer> searchIds;
+	private List<Integer> connectIds;
+
 	private boolean lastSearchWasIPScan = false;
 	private PHHueSDK phHueSDK;
 
 	private LocalBroadcastManager manager;
 
-	public HueService() {
-		super("HueService");
+	private Looper mServiceLooper;
+	private ServiceHandler mServiceHandler;
+
+	// Handler that receives messages from the thread
+	private final class ServiceHandler extends Handler {
+		public ServiceHandler(Looper looper) {
+			super(looper);
+		}
+		@Override
+		public void handleMessage(Message msg) {
+			Bundle b = msg.getData();
+			String command = b.getString(COMMAND);
+			allIds.add(msg.arg1);
+			switch(command){
+				case "randomLights":
+					randomLightIds.add(msg.arg1);
+					randomLights();
+					break;
+				case "doBridgeSearch":
+					searchIds.add(msg.arg1);
+					doBridgeSearch();
+					break;
+				case "connect":
+					connectIds.add(msg.arg1);
+					PHAccessPoint point = b.getParcelable(ACCESS_POINT_EXTRA);
+					connect(point);
+					break;
+				case "disconnect":
+					disconnect();
+					break;
+				default:
+					Log.i(TAG, "non-matching command"+command);
+					stopSelf(msg.arg1);
+			}
+		}
 	}
 
 	@Override
 	public void onCreate(){
-		super.onCreate();
-		phHueSDK = PHHueSDK.create();
+		HandlerThread thread = new HandlerThread("ServiceStartArguments",
+				Process.THREAD_PRIORITY_BACKGROUND);
+		thread.start();
 
+		// Get the HandlerThread's Looper and use it for our Handler
+		mServiceLooper = thread.getLooper();
+		mServiceHandler = new ServiceHandler(mServiceLooper);
+
+		allIds = new ArrayList<Integer>();
+		randomLightIds = new ArrayList<Integer>();
+		searchIds = new ArrayList<Integer>();
+		connectIds = new ArrayList<Integer>();
+
+
+		phHueSDK = PHHueSDK.create();
 		// Set the Device Name (name of your app). This will be stored in your bridge whitelist entry.
 		phHueSDK.setAppName("HueWear");
 		phHueSDK.setDeviceName(android.os.Build.MODEL);
@@ -59,41 +117,40 @@ public class HueService extends IntentService implements PHSDKListener, PHLightL
 	}
 
 	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// For each start request, send a message to start a job and deliver the
+		// start ID so we know which request we're stopping when we finish the job
+
+		Bundle b = new Bundle();
+		b.putString(COMMAND, intent.getStringExtra(COMMAND));
+		b.putParcelable(ACCESS_POINT_EXTRA, intent.getParcelableExtra(ACCESS_POINT_EXTRA));
+		Message msg = mServiceHandler.obtainMessage();
+		msg.arg1 = startId;
+		msg.setData(b);
+
+		mServiceHandler.sendMessage(msg);
+		// If we get killed, after returning from here, restart
+		return START_STICKY;
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// We don't provide binding, so return null
+		return null;
+	}
+
+	@Override
 	public void onDestroy(){
 		super.onDestroy();
 		phHueSDK.getNotificationManager().unregisterSDKListener(this);
 		phHueSDK.disableAllHeartbeat();
 	}
 
-	@Override
-	protected void onHandleIntent(Intent intent) {
-		String command = intent.getStringExtra(COMMAND);
-		if (command.equals("randomLights")) {
-			randomLights();
-		} else if (command.equals("doBridgeSearch")){
-			doBridgeSearch();
-		} else if (command.equals("connect")) {
-			PHAccessPoint point = intent.getParcelableExtra(ACCESS_POINT_EXTRA);
-			connect(point);
-		} else if (command.equals("disconnect")){
-			disconnect();
+	private void stopServices(List<Integer> ids){
+		for (int i : ids){
+			stopSelf(i);
 		}
-	}
-
-	public void randomLights() {
-		PHBridge bridge = phHueSDK.getSelectedBridge();
-
-		List<PHLight> allLights = bridge.getResourceCache().getAllLights();
-		Random rand = new Random();
-
-		for (PHLight light : allLights) {
-			PHLightState lightState = new PHLightState();
-			lightState.setHue(rand.nextInt(MAX_HUE));
-			// To validate your lightstate is valid (before sending to the bridge) you can use:
-			// String validState = lightState.validateState();
-			bridge.updateLightState(light, lightState, this);
-			//  bridge.updateLightState(light, lightState);   // If no bridge response is required then use this simpler form.
-		}
+		ids.clear();
 	}
 
 	public void doBridgeSearch() {
@@ -106,6 +163,7 @@ public class HueService extends IntentService implements PHSDKListener, PHLightL
 		if (!phHueSDK.isAccessPointConnected(lastAccessPoint)) {
 			phHueSDK.connect(lastAccessPoint);
 		}
+		stopServices(connectIds);
 	}
 
 	public void disconnect(){
@@ -127,6 +185,7 @@ public class HueService extends IntentService implements PHSDKListener, PHLightL
 			intent.putParcelableArrayListExtra(POINTS_FOUND, PHAccessPointParcelable.convert(phHueSDK.getAccessPointsFound()));
 			manager.sendBroadcast(intent);
 		}
+		stopServices(randomLightIds);
 	}
 
 	@Override
@@ -141,6 +200,7 @@ public class HueService extends IntentService implements PHSDKListener, PHLightL
 		phHueSDK.getLastHeartbeat().put(b.getResourceCache().getBridgeConfiguration().getIpAddress(), System.currentTimeMillis());
 		Intent intent = new Intent(CONNECT_SUCCESS);
 		manager.sendBroadcast(intent);
+		stopServices(connectIds);
 	}
 
 	@Override
@@ -166,6 +226,7 @@ public class HueService extends IntentService implements PHSDKListener, PHLightL
 		if (!phHueSDK.getDisconnectedAccessPoint().contains(accessPoint)) {
 			phHueSDK.getDisconnectedAccessPoint().add(accessPoint);
 		}
+		stopServices(allIds);
 	}
 
 	@Override
@@ -195,6 +256,7 @@ public class HueService extends IntentService implements PHSDKListener, PHLightL
 			intent.putExtra(ERROR, error);
 			manager.sendBroadcast(intent);
 		}
+		stopServices(allIds);
 	}
 
 	@Override
@@ -205,6 +267,24 @@ public class HueService extends IntentService implements PHSDKListener, PHLightL
 		Intent intent = new Intent(ERROR);
 		intent.putExtra(ERROR, "ParsingError");
 		manager.sendBroadcast(intent);
+		stopServices(allIds);
+	}
+
+	public void randomLights() {
+		PHBridge bridge = phHueSDK.getSelectedBridge();
+
+		List<PHLight> allLights = bridge.getResourceCache().getAllLights();
+		Random rand = new Random();
+
+		for (PHLight light : allLights) {
+			PHLightState lightState = new PHLightState();
+			lightState.setHue(rand.nextInt(MAX_HUE));
+			// To validate your lightstate is valid (before sending to the bridge) you can use:
+			// String validState = lightState.validateState();
+			bridge.updateLightState(light, lightState, this);
+			//  bridge.updateLightState(light, lightState);   // If no bridge response is required then use this simpler form.
+		}
+		stopServices(randomLightIds);
 	}
 
 	//PHLightListener methods
@@ -212,11 +292,13 @@ public class HueService extends IntentService implements PHSDKListener, PHLightL
 	public void onSuccess() {
 		Intent intent = new Intent(LIGHT_SUCCESS);
 		manager.sendBroadcast(intent);
+		stopServices(allIds);
 	}
 
 	@Override
 	public void onStateUpdate(Map<String, String> arg0, List<PHHueError> arg1) {
 		Log.w(TAG, "Light has updated");
+		stopServices(allIds);
 	}
 
 	@Override
